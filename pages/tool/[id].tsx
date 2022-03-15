facbee90987fd17f1e1b1e9b39e2ec061b18614b
dxu <dean.xu@asml.com>
2022-03-15 22:06:49 +0800

    add title

--- pages/tool/[id].tsx
+++ pages/tool/[id].tsx
@@ -3,12 +3,16 @@ import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 import { ToolLayout } from '../../src/components/layout/ToolLayout';
 import { tools } from '../../src/tools';
+import Head from 'next/head'
 
 export default function ToolPage() {
   const router = useRouter();
   const tool = useMemo(() => tools.find(e => e.id === router.query.id)!, [router]);
   return (
     <ToolLayout meta={tool}>
+      <Head>
+        <title>{tool.name} | XDean的百宝箱</title>
+      </Head>
       <tool.content/>
     </ToolLayout>
   );
