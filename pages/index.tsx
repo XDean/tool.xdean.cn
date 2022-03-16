753e288b4fff6b483163f2481592c497d692fca0
dxu <dean.xu@asml.com>
2022-03-16 14:24:37 +0800

    update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,9 @@ const Page = () => {
         {tools.map(meta => (
           <Link key={meta.name} href={`/tool/${meta.id}`}>
             <a className={clsx(
-              'w-[160px] h-[160px] group cursor-pointer rounded border text-center m-2 p-2',
+              'w-[150px] h-[150px] group cursor-pointer rounded border text-center m-2 p-2',
               'transition transform hover:-translate-y-1 hover:ring hover:shadow')}>
-              <div className={'overflow-hidden w-[110px] h-[110px] relative mx-auto'}>
+              <div className={'overflow-hidden w-[100px] h-[100px] relative mx-auto'}>
                 <meta.icon/>
               </div>
               <div className={'group-hover:underline leading-[40px] whitespace-nowrap text-ellipsis overflow-hidden'}
