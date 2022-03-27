0b7d17c5f1761a314f748f1a389a53ba7ee858d6
Dean Xu <373216024@qq.com>
2022-03-27 21:26:46 +0800

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,10 @@ import Head from 'next/head';
 import '../src/styles/global.css';
 import 'common/css/md.css';
 import {BaiduAnalytics} from '../common/util/analytics/baidu';
+import {useGA} from '../common/util/ga';
 
 function MyApp({Component, pageProps}: AppProps) {
+  useGA('G-64T9WP46G4')
   return (
     <MantineProvider emotionOptions={{key: 'mantine', prepend: false}}>
       <Head>
