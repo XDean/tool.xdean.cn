00c86c85a00a44bd1329c106b7e37ccaee68c08b
dxu <dean.xu@asml.com>
2022-04-06 19:12:12 +0800

    fix typo

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://tutorial.xdean.cn',
+  siteUrl: process.env.SITE_URL || 'https://tool.xdean.cn',
   generateRobotsTxt: true,
 }
