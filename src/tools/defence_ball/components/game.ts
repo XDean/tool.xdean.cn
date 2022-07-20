05dd1a97f96a4fcaa654aff20ea0e7beedc83a4d
Dean Xu <373216024@qq.com>
2022-07-20 19:29:17 +0800

    [defence ball] ball play

--- src/tools/defence_ball/components/game.ts
+++ src/tools/defence_ball/components/game.ts
@@ -2,6 +2,7 @@ import * as d3 from 'd3';
 import { range } from '../../../../common/util/array';
 import { randomFloat, randomInt, randomSub } from '../../../../common/util/random';
 import { caseNever } from '../../../../common/util/base';
+import { flip, Vector } from './util';
 
 
 type Cube = ({
@@ -10,13 +11,13 @@ type Cube = ({
 } | {
   type: 'ball'
 }) & {
-  x: number
-  y: number
+  pos: Vector // coord instead of position
 }
 
 type Ball = {
-  x: number // 0 to 14
-  y: number // 0 => top
+  pos: Vector // center
+  speed: Vector
+  wait: number
   die: boolean
 }
 
@@ -35,8 +36,11 @@ const cubeSize = 20;
 const cubePadding = 1;
 const cubeCountPerRow = 15;
 const cubeTextSize = cubeSize / 1.5;
-const ballRadius = 6;
-const bottomY = 380;
+const cubeTextSmallSize = cubeSize / 2;
+const ballRadius = 5;
+const topPadding = cubeSize * 3;
+const bottomY = height - cubeSize;
+const cubeBallCollideSize = cubeSize / 2 - cubePadding + ballRadius;
 
 export function newGame(container: HTMLElement) {
 
@@ -51,110 +55,216 @@ export function newGame(container: HTMLElement) {
   const root = d3.select(container)
     .append('svg')
     .attr('viewBox', '0 0 300 400')
-    .attr('class', 'max-w-screen max-h-screen font-mono bg-black');
+    .attr('class', 'max-w-screen max-h-screen font-mono bg-black select-none');
   const cubes = root.append('g')
-    .attr('id', 'cubes');
+    .attr('id', 'cubes')
+    .attr('transform', `translate(0 ${topPadding})`);
   const balls = root.append('g')
     .attr('id', 'balls');
   const ballHelper = root.append('line')
     .attr('id', 'ball-helper')
     .attr('stroke', 'white');
 
-  function euclideanDistance(a: [number, number], b: [number, number]) {
-    return Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2);
-  }
-
   root.on('pointermove', (ev: PointerEvent) => {
     if (game.state === 'waiting') {
-      const x1 = game.ballStartX;
-      const y1 = bottomY;
-      const [x, y] = d3.pointer(ev);
-      const distance = euclideanDistance([game.ballStartX, bottomY], [x, y]);
-      const ratio = 100 / distance;
-      const x2 = (x - x1) * ratio + x1;
-      const y2 = (y - y1) * ratio + y1;
+      const v1 = Vector.of(game.ballStartX, bottomY);
+      const vp = Vector.of(d3.pointer(ev));
+      const v2 = vp.minus(v1).normalize(100).add(v1);
       ballHelper
+        .attr('visibility', 'visible')
         .attr('stroke-dasharray', 10)
         .attr('stroke-dashoffset', 10)
-        .attr('x1', x1)
-        .attr('y1', y1)
-        .attr('x2', x2)
-        .attr('y2', y2);
+        .attr('x1', v1.x)
+        .attr('y1', v1.y)
+        .attr('x2', v2.x)
+        .attr('y2', v2.y);
+    }
+  });
+
+  root.on('click', (ev: MouseEvent) => {
+    if (game.state === 'waiting') {
+      const v1 = Vector.of(game.ballStartX, bottomY);
+      const v = Vector.of(d3.pointer(ev));
+      const speed = v.minus(v1).normalize(400);
+      game.balls.forEach(b => b.speed = speed);
+      game.state = 'running';
+      ballHelper
+        .attr('visibility', 'hidden');
     }
   });
 
   function newLevel() {
     game.state = 'waiting';
     game.level += 1;
-    game.cubes.forEach(e => e.y += 1);
-    randomSub(range(cubeCountPerRow), randomInt(cubeCountPerRow * 0.4, cubeCountPerRow * 0.8 + 1)).forEach((e) => {
-      game.cubes.push({
+    game.cubes.forEach(e => e.pos = e.pos.add(Vector.of(0, 1)));
+    const newCubes: Cube[] = [];
+    randomSub(range(cubeCountPerRow), randomInt(cubeCountPerRow * 0.4, cubeCountPerRow * 0.9)).forEach((e) => {
+      let count = Math.ceil(game.level * randomFloat(0.6, 1.2));
+      if (randomFloat() < 0.1) {
+        count += Math.ceil(game.level * 0.5 + game.ballCount);
+      }
+      newCubes.push({
         type: 'cube',
-        count: Math.ceil(game.level * randomFloat(0.5, 20)),
-        x: e,
-        y: 0,
+        count: count,
+        pos: Vector.of(e, 0),
       });
     });
+    if (randomFloat() < 0.1 + 1 / Math.sqrt(game.level)) {
+      const idx = randomInt(newCubes.length);
+      newCubes[idx].type = 'ball';
+    }
+    game.cubes.push(...newCubes);
     game.ballStartX = randomFloat(0.2, 0.8) * width;
-    game.balls = range(game.ballCount).map(() => ({
+    game.balls = range(game.ballCount).map((i) => ({
+      pos: Vector.of(game.ballStartX, bottomY),
+      speed: Vector.zero,
+      wait: i * 100,
       die: false,
-      x: game.ballStartX,
-      y: bottomY,
     }));
+
+    renderCubes();
+    renderBalls();
   }
 
-  function collideTest() {
+  function move(totalMillis: number) {
+    let needRenderCube = false;
+    let leftMillis = totalMillis;
+    while (leftMillis > 0) {
+      const millis = Math.min(leftMillis, 50);
+      leftMillis -= millis;
+      game.balls
+        .filter(e => !e.die && e.wait < millis)
+        .forEach(b => {
+          let [x, y] = b.pos.add(b.speed.multi((millis - b.wait) / 1000)).array;
+          let [sx, sy] = b.speed.array;
 
+          // world collide
+          if (x < ballRadius) {
+            x = flip(x, ballRadius);
+            sx = -sx;
+          } else if (x > width - ballRadius) {
+            x = flip(x, width - ballRadius);
+            sx = -sx;
+          }
+          if (y < ballRadius) {
+            y = flip(y, ballRadius);
+            sy = -sy;
+          } else if (y > bottomY) {
+            // ball die
+            y = bottomY;
+            sx = 0;
+            sy = 0;
+            b.die = true;
+          }
+
+          // cube collide
+          game.cubes = game.cubes.filter(e => {
+            const cx = (e.pos.x + 0.5) * cubeSize;
+            const cy = (e.pos.y + 0.5) * cubeSize + topPadding;
+            if (
+              Math.abs(x - cx) < cubeBallCollideSize &&
+              Math.abs(y - cy) < cubeBallCollideSize
+            ) {
+              needRenderCube = true;
+              if (e.type === 'ball') {
+                game.ballCount += 1;
+                return false;
+              } else {
+                e.count -= 1;
+                const rel = Vector.of(cx, cy).minus(b.pos);
+                if (rel.y > rel.x) {
+                  if (rel.y > -rel.x) {
+                    // collide cube top
+                    y = flip(y, cy - cubeSize / 2 + cubePadding - ballRadius);
+                    sy = -sy;
+                  } else {
+                    // collide cube right
+                    x = flip(x, cx + cubeSize / 2 - cubePadding + ballRadius);
+                    sx = -sx;
+                  }
+                } else {
+                  if (rel.y > -rel.x) {
+                    // collide cube left
+                    x = flip(x, cx - cubeSize / 2 + cubePadding - ballRadius);
+                    sx = -sx;
+                  } else {
+                    // collide cube bottom
+                    y = flip(y, cy + cubeSize / 2 - cubePadding + ballRadius);
+                    sy = -sy;
+                  }
+                }
+                return e.count > 0;
+              }
+            }
+            return true;
+          });
+          b.pos = Vector.of(x, y);
+          b.speed = Vector.of(sx, sy);
+        });
+      game.balls.forEach(e => e.wait = Math.max(0, e.wait - millis));
+    }
+    if (needRenderCube) {
+      renderCubes();
+    }
   }
 
-  function render() {
-    // render cubes
+  function renderCubes() {
     const gs = cubes
       .selectAll('g')
       .data(game.cubes)
       .join('g')
-      .attr('transform', e => `translate(${e.x * cubeSize} ${e.y * cubeSize})`);
+      .attr('id', e => `${e.pos.x}-${e.pos.y}`)
+      .attr('transform', e => `translate(${e.pos.x * cubeSize} ${e.pos.y * cubeSize})`);
     gs.selectAll('rect')
       .data(e => [e])
       .join('rect')
       .attr('x', cubePadding)
       .attr('y', cubePadding)
+      .attr('rx', e => e.type === 'ball' ? '100' : '')
       .attr('width', cubeSize - cubePadding * 2)
       .attr('height', cubeSize - cubePadding * 2)
-      .attr('fill', e => e.type === 'cube' ? d3.interpolateSinebow((e.count % 13) / 13) : '');
+      .attr('fill', e => e.type === 'cube' ? d3.interpolateRainbow(1 - Math.sqrt(e.count) /10) : 'white');
     gs.selectAll('text')
       .data(e => [e])
       .join('text')
       .text(e => e.type === 'cube' ? e.count : '+1')
       .attr('x', cubeSize / 2)
       .attr('y', cubeSize / 2)
-      .attr('font-size', cubeTextSize)
+      .attr('font-size', e => e.type === 'cube' && e.count >= 100 ? cubeTextSmallSize : cubeTextSize)
       .attr('text-anchor', 'middle')
       .attr('dominant-baseline', 'central');
+  }
 
-    // render balls
+  function renderBalls() {
     balls.selectAll('circle')
       .data(game.balls)
       .join('circle')
       .attr('r', ballRadius)
-      .attr('cx', e => e.x)
-      .attr('cy', e => e.y)
+      .attr('cx', e => e.pos.x)
+      .attr('cy', e => e.pos.y)
       .attr('fill', 'white');
-
   }
 
   newLevel();
 
   let handle = -1;
-  const frame = () => {
+  let lastTime = -1;
+  const frame = (time: number) => {
     handle = requestAnimationFrame(frame);
-    render();
+    if (lastTime === -1) {
+      lastTime = time;
+      return;
+    }
+    const timeSpent = time - lastTime;
+    lastTime = time;
+
     switch (game.state) {
       case 'running':
         if (game.balls.every(e => e.die)) {
           newLevel();
         } else {
-          collideTest();
+          move(timeSpent);
+          renderBalls();
         }
         break;
       case 'waiting':
---
+++ src/tools/defence_ball/components/util.ts
@@ -0,0 +1,53 @@
+export class Vector {
+
+  static of(x: number, y: number): Vector;
+  static of(xy: [number, number]): Vector;
+  static of(xy: number | [number, number], y?: number): Vector {
+    if (Array.isArray(xy)) {
+      return new Vector(xy[0], xy[1]);
+    } else {
+      return new Vector(xy, y!);
+    }
+  }
+
+  static zero = new Vector(0, 0);
+
+  constructor(
+    readonly x: number,
+    readonly y: number,
+  ) {
+
+  }
+
+  distance(v: Vector) {
+    return Math.sqrt((this.x - v.x) ** 2 + (this.y - v.y) ** 2);
+  }
+
+  normalize(len: number = 1): Vector {
+    return this.multi(len / this.length());
+  }
+
+  length() {
+    return this.distance(Vector.zero);
+  }
+
+  multi(f: number): Vector {
+    return Vector.of(this.x * f, this.y * f);
+  }
+
+  add(v: Vector): Vector {
+    return Vector.of(this.x + v.x, this.y + v.y);
+  }
+
+  minus(v: Vector): Vector {
+    return Vector.of(this.x - v.x, this.y - v.y);
+  }
+
+  get array(): [number, number] {
+    return [this.x, this.y];
+  }
+}
+
+export function flip(value: number, base: number) {
+  return base + (base - value);
+}
