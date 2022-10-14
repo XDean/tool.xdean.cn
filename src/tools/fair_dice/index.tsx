f1f14f9bf5e9fbe73f88faa4613a5ebbff7e8b3d
dxu <dean.xu@asml.com>
2022-10-14 17:05:08 +0800

    [fair-dice] introduce.mdx

--- src/tools/fair_dice/index.tsx
+++ src/tools/fair_dice/index.tsx
@@ -1,14 +1,24 @@
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
-import { FunctionComponent } from 'react';
+import React, {FunctionComponent} from 'react';
 import logo from '../../../public/tool/fair_dice/logo_512.webp';
-import { Tool } from '../index';
+import {Tool} from '../index';
+import Introduce from './introduce.mdx';
 
 const Main = dynamic(() => import('./components/Main').then<FunctionComponent>(e => e.Main));
 
 export const FairDice: Tool = {
   id: 'fair-dice',
   name: '公平的骰子',
-  icon: () => <Image src={logo} layout={'responsive'}/>,
+  icon: () => (
+    <Image src={logo}
+           layout={'responsive'}
+    />
+  ),
   content: Main,
+  details: () => (
+    <div className={'markdown-body'}>
+      <Introduce/>
+    </div>
+  ),
 };
