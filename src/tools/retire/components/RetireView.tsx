95f662374c4ee6831fd60f17b0a69025ea5ef654
Dean Xu <373216024@qq.com>
2025-04-20 21:29:26 +0800

    [retire] add read/like

--- src/tools/retire/components/RetireView.tsx
+++ src/tools/retire/components/RetireView.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { ToolLayout } from '../../../components/layout/ToolLayout';
 import { Retire, RetireRes } from '../model/retire';
 import { RetireInputForm } from './RetireInputForm';
@@ -6,12 +6,17 @@ import { RetireResView } from './RetireResView';
 import { Button } from '@mantine/core';
 import { Achievement, BPZJ, calcAchievement } from '../model/achievement';
 import { AchievementView } from './AchievementView';
-import { RetireHelpDialog } from './RetireHelpDialog'; // 新增：引入帮助对话框组件
+import { RetireHelpDialog } from './RetireHelpDialog';
+import { ReadWithAPI } from '../../../../common/components/badge/Read';
+import { GuoBiao } from '../../guobiao';
+import { LikeWithAPI } from '../../../../common/components/badge/Like';
+import { GithubComment } from '../../../components/util/GithubComment'; // 新增：引入帮助对话框组件
 
 export const RetireView = () => {
   const [input, setInput] = useState(Retire.defaultInput());
   const [res, setRes] = useState<RetireRes>();
   const [achievements, setAchievements] = useState<Achievement[]>([]);
+  const commentRef = useRef<HTMLDivElement>(null);
 
   return (
     <ToolLayout
@@ -22,6 +27,11 @@ export const RetireView = () => {
       ads={false}
     >
       <div className={'text-sm space-y-2'}>
+        <div className={'space-x-2 flex items-center justify-center w-full'}>
+          <ReadWithAPI id={`tool:${GuoBiao.id}`} name={'访问'}/>
+          <LikeWithAPI id={`tool:${GuoBiao.id}`}/>
+        </div>
+        <hr/>
         <RetireInputForm
           value={input}
           onChange={(v) => {
@@ -64,7 +74,10 @@ export const RetireView = () => {
             />
           </div>
         )}
-
+        <hr/>
+        <div className={'w-full'} ref={commentRef}>
+          <GithubComment/>
+        </div>
       </div>
     </ToolLayout>
   );
