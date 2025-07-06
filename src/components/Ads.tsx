d4bb5f5bd398946086706eb6b08dd2ce5452c3c8
Dean Xu <373216024@qq.com>
2025-07-06 18:07:34 +0800

    remove ads

--- src/components/Ads.tsx
+++ src/components/Ads.tsx
@@ -1,23 +1,22 @@
-import { useEffect } from 'react';
-
 export const Ads = () => {
-  useEffect(() => {
-    const w = window as any;
-    try {
-      (w.adsbygoogle = w.adsbygoogle || []).push({});
-    } catch (e) {
-      console.log('Ads failed', e);
-    }
-  }, []);
-
-
-  return (
-    <ins className={'adsbygoogle'}
-         style={{display: 'block'}}
-         data-ad-client={'ca-pub-7804430263218291'}
-         data-ad-slot={'6714809866'}
-         data-ad-format={'auto'}
-         data-full-width-responsive={'true'}
-    />
-  );
+  return null;
+  // useEffect(() => {
+  //   const w = window as any;
+  //   try {
+  //     (w.adsbygoogle = w.adsbygoogle || []).push({});
+  //   } catch (e) {
+  //     console.log('Ads failed', e);
+  //   }
+  // }, []);
+  //
+  //
+  // return (
+  //   <ins className={'adsbygoogle'}
+  //        style={{display: 'block'}}
+  //        data-ad-client={'ca-pub-7804430263218291'}
+  //        data-ad-slot={'6714809866'}
+  //        data-ad-format={'auto'}
+  //        data-full-width-responsive={'true'}
+  //   />
+  // );
 };
